1714b4874b295fd4c6830f00d5f74036cca1662f
Beke Tamás <beke07@gmail.com>
2021-03-27 12:59:30 +0100

    feat: add commands

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -9,5 +9,22 @@ namespace CommanderGQL.Data
         { }
 
         public DbSet<Platform> Platforms { get; set; }
+
+        public DbSet<Command> Commands { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Platform>()
+                .HasMany(p => p.Commands)
+                .WithOne(c => c.Platform)
+                .HasForeignKey(c => c.PlatformId);
+
+            modelBuilder
+                .Entity<Command>()
+                .HasOne(c => c.Platform)
+                .WithMany(p => p.Commands)
+                .HasForeignKey(c => c.PlatformId);
+        }
     }
 }
